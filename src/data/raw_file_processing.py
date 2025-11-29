f07e3c75ca3b45a3fa31c4beb6264c1640c17c1b
Joelaniado <jlaniado.g@gmail.com>
2025-11-29 14:32:10 +0000

    feat(src/data/raw_file_processing.py): added polars pipeline for combining each month of airline data into single parquet file.

--- src/data/raw_file_processing.py
+++ src/data/raw_file_processing.py
@@ -1,12 +1,26 @@
 import os
 import glob
+import time
+import sys
+sys.path.append(os.getcwd())
+
+import pandas as pd
+import polars as pl
 
 from src.utils.paths import DATA_DIR
 
 def main():
-    directory = DATA_DIR / "raw" / "airline_data"
-    for filename in glob.glob(f"{directory}/*"):
-        print(filename)
+
+    start_time = time.time()
+
+    directory = DATA_DIR / "raw" / "airline_data" / "*.csv"
+    files = glob.glob(str(directory))
+    dfs = [pl.scan_csv(file) for file in files]
+    final_df = pl.concat(dfs).collect()
+    final_df.write_parquet(DATA_DIR / "raw"/ "airline_data" / "2024-airline-data.parquet")
+
+    print(f'Run time: {time.time()-start_time:.4f}')
+        
 
 if __name__ == "__main__":
     main()
