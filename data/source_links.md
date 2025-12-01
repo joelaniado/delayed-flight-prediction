a88cb2ce0ebe24fd9a4ca9dbd9376260e392edd2
Joelaniado <jlaniado.g@gmail.com>
2025-12-01 19:04:20 +0000

    feat(src/data/raw_file_processing): added function for reading in weather data.

--- data/source_links.md
+++ data/source_links.md
@@ -1,3 +1,4 @@
 # Data sources:
 Aviation Weather Center API: https://aviationweather.gov/data/api/
 Bureau of Transportation Statistics: https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGJ&QO_fu146_anzr=b0-gvzr
+Weather data - national centers for environmental information ISD dataset: https://www.ncei.noaa.gov/products/land-based-station/integrated-surface-database
--- src/data/raw_file_processing.py
+++ src/data/raw_file_processing.py
@@ -9,15 +9,27 @@ import polars as pl
 
 from src.utils.paths import DATA_DIR
 
-def main():
-
-    start_time = time.time()
-
+def airline_files_merge():
     directory = DATA_DIR / "raw" / "airline_data" / "*.csv"
     files = glob.glob(str(directory))
     dfs = [pl.scan_csv(file) for file in files]
     final_df = pl.concat(dfs).collect()
     final_df.write_parquet(DATA_DIR / "raw"/ "airline_data" / "2024-airline-data.parquet")
+
+def raw_weather_data_processing():
+    data_path = DATA_DIR / "raw" / "weather-data" / "raw_weather_data.csv"
+    df = pd.read_csv(data_path)
+    print(f'{df.info()}\n{df.head()}')
+
+    
+
+def main():
+
+    start_time = time.time()
+    #airline_file_merge()
+    raw_weather_data_processing()
+
+    
 
     print(f'Run time: {time.time()-start_time:.4f}')
         
